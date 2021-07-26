dda17c3a4c5096f232181e02d4e1814e7698e518
natalie813 <87865330+natalie813@users.noreply.github.com>
2021-07-26 15:19:26 +0800

    Update 开盘动量_evaluation.py
    夏普比率实际中将无风险收益率看做0

--- 开盘动量_evaluation.py
+++ 开盘动量_evaluation.py
@@ -12,7 +12,7 @@ def evaluation(pd_buy,pd_sell,data):
     
     # Sharpe Ratio：收益均值/标准差
     # ???????
-    Sharpe_ratio = (data.return_after * data.flag-0.03/365).mean() / (data.return_after * data.flag).std() * np.sqrt(N) #np.sqrt函数计算每个元素的开方
+    Sharpe_ratio = (data.return_after * data.flag).mean() / (data.return_after * data.flag).std() * np.sqrt(N) #np.sqrt函数计算每个元素的开方
     
     # win rate，(trans.price_sell - trans.price_buy) > 0为真返回1，假返回0
     r_buy = len(pd_buy.price_buy)/(len(pd_buy.price_buy)+len(pd_sell.price_sell))
